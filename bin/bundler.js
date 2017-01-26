fd4fdd245e473a863b8ff1ef1e015690eb319446
Erik Sundell <erik.sundell87@gmail.com>
2017-01-26 08:42:56 +0100

    ref

--- bin/bundler.js
+++ bin/bundler.js
@@ -8,7 +8,9 @@ var builder = new SystemBuilder();
 
 builder.loadConfig('./public/js/systemjs.config.js')
   .then(function(){
+    process.stdout.write('bundle started');
     var outputFile = argv.prod ? './public/js/bundle.min.js' : './public/js/bundle.js';
+    process.stdout.write(outputFile);
     return builder.buildStatic('app', outputFile, {
       minify: argv.prod,
       mangle: argv.prod,
@@ -16,11 +18,12 @@ builder.loadConfig('./public/js/systemjs.config.js')
     });
   }) 
   .then(function() {
-
     const gzip = zlib.createGzip();
 
     const inp = fs.createReadStream(path.resolve(__dirname + '/../public/js/bundle.min.js'));
     const out = fs.createWriteStream(path.resolve(__dirname + '/../public/js/bundle.min.js.gz'));
+
+    process.stdout.write(out);
 
     inp.pipe(gzip).pipe(out);
     console.log(path.resolve(__dirname + '/../public/js/bundle.min.js'));
