fc60ad3b156e21795c31b7151458eca830d5ab04
Erik Sundell <erik.sundell87@gmail.com>
2017-01-28 16:55:16 +0100

    REf

---
+++ app/dateAndTimeSelector/dateAndTimeSelector.component.ts
@@ -0,0 +1,60 @@
+﻿import { Component, OnInit, Input, ViewChild, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Club, User, TimePickerSettings, TimeSlot, ClubSlots } from '../_models/index';
+import { ClubService, UserSlotPreference, AlertService, TimePickerSettingsFactory } from '../_services/index';
+import { TimeSlotSelectorComponent } from '../timeSlotSelector/index';
+
+@Component({
+    selector: 'date-and-time-selector',
+    moduleId: module.id,
+    templateUrl: 'dateAndTimeSelector.component.html'
+})
+
+export class DateAndTimeSelectorComponent implements AfterViewInit {
+    @Input() clubSlots: any;
+    model: any = {};
+    currentUser: User;
+    loading = false;
+
+    constructor(
+        private router: Router,
+        private userSlotPreference: UserSlotPreference,
+        private alertService: AlertService,
+        private timePickerSettingsFactory: TimePickerSettingsFactory) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentTennisTiderUser'));
+    }
+
+    ngOnChanges(changes: any): void {
+        var settingsChange = changes.clubSlots.currentValue;
+        if (settingsChange) {
+        }
+    }
+
+    @ViewChildren(TimeSlotSelectorComponent) child: QueryList<TimeSlotSelectorComponent>;
+
+    ngAfterViewInit() {
+        this.child.forEach(child => child.refresh());
+    }
+
+    // save() {
+    //     let slots: TimeSlot[] = [];
+    //     if (!this.clubSlots.morningSettings.excluded) {
+    //         slots = slots.concat(this.morningSettings.timeSlot);
+    //     }
+    //     if (!this.clubSlots.eveningSettings.excluded) {
+    //         slots = slots.concat(this.eveningSettings.timeSlot);
+    //     }
+    //     if (!this.clubSlots.lunchSettings.excluded) {
+    //         slots = slots.concat(this.lunchSettings.timeSlot);
+    //     }
+    //     if (!this.clubSlots.weekendSettings.excluded) {
+    //         slots = slots.concat(this.weekendSettings.timeSlot);
+    //     }
+
+    //     this.userSlotPreference.saveDefaultTimeSlotSettings(this.currentUser.email, slots)
+    //         .subscribe((data) => {
+    //             this.router.navigateByUrl('/home');
+    //         });
+    // }
+}
---
+++ app/slotMonitor/slotMonitor.component.ts
@@ -0,0 +1,42 @@
+﻿import { AfterViewInit, Directive, Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { Club, User, TimePickerSettings, TimeSlot, ClubSlots } from '../_models/index';
+import { ClubService, UserSlotPreference, AlertService, TimePickerSettingsFactory, UserService } from '../_services/index';
+
+@Component({
+    moduleId: module.id,
+    templateUrl: 'slotMonitor.component.html'
+})
+
+export class SlotMonitorComponent {
+    currentUser: User;
+    userObj: any;
+    clubs: Club[];
+
+    constructor(
+        private router: Router,
+        private userSlotPreference: UserSlotPreference,
+        private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentTennisTiderUser'));
+        this.userService.getByEmail(this.currentUser.email)
+            .subscribe((user: any) => {
+                if (user.success === false) {
+                    this.router.navigateByUrl('/logga-in');
+                }
+                this.userObj = user;
+                this.clubs = user.slotPreference.map((x: any) => x.club);
+            });
+    }
+
+    save() {
+        // this.userSlotPreference.saveClubTimeSlotSettings(this.currentUser.email, this.clubSlots)
+        //     .subscribe((data) => {
+        //         if (this.userObj.firstTimeUser) {
+        //             this.router.navigateByUrl('/valj-bastid');
+        //         } else {
+        //             this.router.navigateByUrl('/home');
+        //         }
+        //     });
+    }
+}
