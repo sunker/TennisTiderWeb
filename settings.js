2d14e32a72311431ae220748e036114d9fd0b25a
Erik Sundell <erik.sundell87@gmail.com>
2017-03-01 07:46:22 +0100

    ref

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 var dev = process.env.NODE_ENV === 'debug';
 module.exports = Object.freeze({
     minDelay: dev ? 500 : 6000,
-    maxDelay: dev ? 1500 : 25000,
+    maxDelay: dev ? 1500 : 20000,
 });
