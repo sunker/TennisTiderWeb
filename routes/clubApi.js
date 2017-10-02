85c9a909a983802d3fe9cb4d1cd763d0e3c63f4e
Erik Sundell <erik.sundell87@gmail.com>
2017-10-02 19:34:54 +0200

    Add new clubs

--- routes/clubApi.js
+++ routes/clubApi.js
@@ -1,10 +1,11 @@
 var express = require('express'),
-    router = express.Router(),
-    clubService = require('../services/clubService'),
-    mongoose = require('mongoose'),
-    jwtAuthentication = require('../middleware/jwtAuthentication'),
-    // noCacheHeader = require('../middleware/noCacheHeader'),
-    User = mongoose.model('user');
+  router = express.Router(),
+  clubService = require('../services/clubService'),
+  mongoose = require('mongoose'),
+  jwtAuthentication = require('../middleware/jwtAuthentication'),
+  rp = require('request-promise'),
+  User = mongoose.model('user'),
+  request = require('request');
 
 router.use(jwtAuthentication);
 // router.use(noCacheHeader);
@@ -12,25 +13,28 @@ router.use(jwtAuthentication);
 router.use(jwtAuthentication);
 
 router.get('/list', function (req, res) {
-    var result = clubService.getAllClubs();
-    return res.end(JSON.stringify(_.sortBy(result, 'name')));
+  request(`https://tennistider-api.herokuapp.com/api/club/list`, function (error, response, html) {
+    return res.end(JSON.stringify(_.sortBy(JSON.parse(response.body), 'name')));
+  })
 });
 
 router.get('/withUserInfo/:email', function (req, res) {
-    var result = clubService.getAllClubs();
+  request(`https://tennistider-api.herokuapp.com/api/club/list`, function (error, response, html) {
+    var result = JSON.parse(response.body);
     User.getByEmail(req.params.email).then((user) => {
-        const userObj = user.toObject();
-        result.forEach((club) => {
-            const exist = userObj.slotPreference.find(x => x.clubId === club.id);
-            if (exist) {
-                club.active = true;
-            } else {
-                club.active = false;
-            }
-        });
-        
-        return res.end(JSON.stringify(_.sortBy(result, 'name')));
+      const userObj = user.toObject();
+      result.forEach((club) => {
+        const exist = userObj.slotPreference.find(x => x.clubId === club.id);
+        if (exist) {
+          club.active = true;
+        } else {
+          club.active = false;
+        }
+      });
+
+      return res.end(JSON.stringify(_.sortBy(result, 'name')));
     });
+  });
 });
 
 module.exports = router;
